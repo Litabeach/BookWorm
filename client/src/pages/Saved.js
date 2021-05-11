ef8f6a3d6b3a6c564aaeba3d365fb63218e9e3c3
Lita Beach <litabeach123@gmail.com>
2021-05-11 16:38:21 -0500

    book save working

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -103,3 +103,5 @@ const Saved = () => {
 }
 
 export default Saved;
+
+
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -3,9 +3,9 @@ const db = require("../models");
 
 // This file empties the Books collection and inserts the books below
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
-  useNewUrlParser: true,
-});
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+//   useNewUrlParser: true,
+// });
 
 const bookSeed = [
   {
@@ -18,7 +18,7 @@ const bookSeed = [
 ];
 
 db.Book
-  .remove({})
+  .deleteOne({})
   .then(() => db.Book.collection.insertMany(bookSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
