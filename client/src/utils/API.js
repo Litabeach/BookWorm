be32bd88971b41e82d7045332e55fc7ef5532146
Lita Beach <litabeach123@gmail.com>
2021-05-11 12:40:59 -0500

    updated Google Books API with URL

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,22 @@
 import axios from "axios";
+const URL = "https://www.googleapis.com/books/v1/volumes?q=";
 
 export default {
   // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
+  getBooks: title => {
+    return axios.get(URL + title);
   },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
-  },
-  // Deletes the book with the given id
-  deleteBook: function(id) {
+ // Gets all saved books
+ getSavedBooks: () => {
+	return axios.get("/api/books/");
+},
+   // Deletes the book with the given id
+   deleteBook: (id) => {
     return axios.delete("/api/books/" + id);
   },
+
   // Saves a book to the database
-  saveBook: function(bookData) {
+  saveBook: bookData => {
     return axios.post("/api/books", bookData);
   }
 };
