659b394e56c786834fe2f60e43941b3b72cb5e67
Lita Beach <litabeach123@gmail.com>
2021-05-11 13:46:40 -0500

    Search function and handleInputchange created

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -4,47 +4,60 @@ import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
 
-function Detail(props) {
-  const [book, setBook] = useState({})
 
-  // When this component mounts, grab the book with the _id of props.match.params.id
-  // e.g. localhost:3000/books/599dcb67f0f16317844583fc
-  const {id} = useParams()
-  useEffect(() => {
-    API.getBook(id)
-      .then(res => setBook(res.data))
-      .catch(err => console.log(err));
-  }, [])
+const Search = () => {
+  // Setting our component's initial state
+  const [books, setBooks] = useState([]);
+  const [searchBook, setSearchBook] = useState("");
 
-  return (
-      <Container fluid>
-        <Row>
-          <Col size="md-12">
-            <Jumbotron>
-              <h1>
-                {book.title} by {book.author}
-              </h1>
-            </Jumbotron>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-10 md-offset-1">
-            <article>
-              <h1>Synopsis</h1>
-              <p>
-                {book.synopsis}
-              </p>
-            </article>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-2">
-            <Link to="/">← Back to Authors</Link>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+  
+  // Handles updating component state when the user types into the input field
+  const handleInputChange = e => {
+    const { value } = e.target;
+    setSearchBook(value);
+  };
+
+// function Detail(props) {
+//   const [book, setBook] = useState({})
+
+//   // When this component mounts, grab the book with the _id of props.match.params.id
+//   // e.g. localhost:3000/books/599dcb67f0f16317844583fc
+//   const {id} = useParams()
+//   useEffect(() => {
+//     API.getBook(id)
+//       .then(res => setBook(res.data))
+//       .catch(err => console.log(err));
+//   }, [])
+
+//   return (
+//       <Container fluid>
+//         <Row>
+//           <Col size="md-12">
+//             <Jumbotron>
+//               <h1>
+//                 {book.title} by {book.author}
+//               </h1>
+//             </Jumbotron>
+//           </Col>
+//         </Row>
+//         <Row>
+//           <Col size="md-10 md-offset-1">
+//             <article>
+//               <h1>Synopsis</h1>
+//               <p>
+//                 {book.synopsis}
+//               </p>
+//             </article>
+//           </Col>
+//         </Row>
+//         <Row>
+//           <Col size="md-2">
+//             <Link to="/">← Back to Authors</Link>
+//           </Col>
+//         </Row>
+//       </Container>
+//     );
+//   }
 
 
-export default Detail;
+// export default Detail;
