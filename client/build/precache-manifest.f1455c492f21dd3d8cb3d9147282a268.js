435fd507ff99a44bc6b0088ea911d5b8ac7f4cb9
Lita Beach <litabeach123@gmail.com>
2021-05-13 12:56:05 -0500

    added build

---
+++ client/build/precache-manifest.f1455c492f21dd3d8cb3d9147282a268.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73286d2c4d5125fd2ae430670c3f8eb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cc61f0ab66fd40cf7c9",
+    "url": "/static/css/main.7f4d713f.chunk.css"
+  },
+  {
+    "revision": "77d75884f5a5d88db95c",
+    "url": "/static/js/2.2a6986f1.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.2a6986f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cc61f0ab66fd40cf7c9",
+    "url": "/static/js/main.99048cd7.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
